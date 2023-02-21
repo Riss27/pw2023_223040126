c43329149cc17a09b8966c94cf37d20b8d8f4453
Riss27 <faris.223040126@mail.unpas.ac.id>
2023-02-21 23:40:36 +0700

    tugas 1c

--- tugas/tugas1/tugas1c.php
+++ tugas/tugas1/tugas1c.php
@@ -5,8 +5,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>tugas1c</title>
+    <style>
+        .box {
+            width: 40px;
+            background-color: navajowhite;
+            text-align: center;
+            border: 1px solid salmon;
+            line-height: 40px;
+            aspect-ratio: 1;
+        }
+
+        .box_wrapper {
+            display: flex;
+        }
+    </style>
 </head>
+
 <body>
-    
+    <div class="box">1</div>
+    <div class="box-wrapper">
+        <div class="box">2</div>
+        <div class="box">2</div>
+    </div>
+    <div class="box-wrapper">
+        <div class="box">3</div>
+        <div class="box">3</div>
+        <div class="box">3</div>
+    </div>
 </body>
 </html>
